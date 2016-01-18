56f31637cf91756f19d1ab8d6d22874f8bfde6c2
zidein07 <zidein07@gmail.com>
2016-01-18 22:26:46 +0400

    twig tpl

--- twig/index.php
+++ twig/index.php
@@ -5,8 +5,9 @@ require_once '../vendor/autoload.php';
 $loader = new Twig_Loader_Array([
     'index' => 'Hello {{ name }}!',
 ]);
+
 $twig = new Twig_Environment($loader);
 
 echo $twig->render('index', [
-    'name' => 'Fabien'
+    'name' => 'Stas'
 ]);
