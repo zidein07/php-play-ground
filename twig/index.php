230a04d189494553e65df435dff139f6b3e19d45
zidein07 <zidein07@gmail.com>
2016-01-18 22:37:03 +0400

    twig loader filesystem

--- twig/index.php
+++ twig/index.php
@@ -2,12 +2,21 @@
 
 require_once '../vendor/autoload.php';
 
-$loader = new Twig_Loader_Array([
-    'index' => 'Hello {{ name }}!',
+//$loader = new Twig_Loader_Array([
+//    'index' => 'Hello {{ name }}!',
+//]);
+//
+//$twig = new Twig_Environment($loader);
+//
+//echo $twig->render('index', [
+//    'name' => 'Stas'
+//]);
+
+$loader = new Twig_Loader_Filesystem('./views');
+
+$twig = new Twig_Environment($loader, [
+    'debug' => true,
+    'cache' => './compilation_cache',
 ]);
 
-$twig = new Twig_Environment($loader);
-
-echo $twig->render('index', [
-    'name' => 'Stas'
-]);
+echo $twig->render('index.twig', ['name' => 'Fabien']);
